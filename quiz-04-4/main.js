d1c2d228589f17e79d740e61bf90a4d256556264
fatemeh ezzati <fatemehezzati121@gmail.com>
2023-01-27 11:57:51 +0330

    ex4

--- quiz-04-4/main.js
+++ quiz-04-4/main.js
@@ -2,34 +2,49 @@ const tableHead = document.querySelector('thead');
 const tableBody = document.querySelector('tbody');
 
 const renderTable = () => {
+
+	const url_string = window.location.href;
+	const url = new URL(url_string);
+
+	let searchElement = ''
+	let searchVal = ''
+	let index = ''
+	let list = Object.keys(tours[0])
+
+	for (let i = 0; i < list.length; i++) {
+		let obj = list[i]
+		if (url.searchParams.get(obj)) {
+			searchElement = obj
+			searchVal = url.searchParams.get(obj)
+			index = i
+		}
+	}
+
+	console.log(searchElement, searchVal)
+
+	
 	tableHead.innerHTML = `
 		<tr>
-			<th scope="col">No.</th>
-			<th scope="col">Tour ID</th>
-			<th scope="col">Name</th>
-			<th scope="col">Location</th>
-			<th scope="col">Price</th>
-			<th scope="col">Group Size</th>
-			<th scope="col">difficulty</th>
-			<th scope="col">duration</th>
-			<th scope="col">Ratings Average</th>
-			<th scope="col">Ratings Quantity</th>
+		<th scope="col">No.</th>
+		${Object.keys(tours[0]).map(item=>{
+			return `<th scope="col">${item}</th>`
+		}).join('')}
 		</tr>`;
 
 	let rowCount = 1;
 	for (const tour of tours) {
 		tableBody.innerHTML += `
 			<tr>
-				<th scope="row">${rowCount}</th>
-				<td>${tour.id}</td>
-				<td>${tour.name}</td>
-				<td>${tour.location}</td>
-				<td>${tour.price}</td>
-				<td>${tour.maxGroupSize}</td>
-				<td>${tour.difficulty}</td>
-				<td>${tour.duration}</td>
-				<td>${tour.ratingsAverage}</td>
-				<td>${tour.ratingsQuantity}</td>
+			<th scope="row">${rowCount}</th>
+			${Object.values(tour).map(data => { 
+				console.log('vall====',Object.values(tour)[index])
+				if(searchElement && Object.values(tour)[index] === searchVal){
+					return (`<td >${data}</td>`)
+				}
+				return ''
+				 }).join('')
+			
+			}
 			</tr>`;
 
 		rowCount += 1;
